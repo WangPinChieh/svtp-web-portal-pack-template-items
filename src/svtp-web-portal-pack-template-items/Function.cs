5fc5e9069f716e9dcb70e60c2c65daf13a9b0bc6
Jay Wang <jay.wang@hp.com>
2022-03-24 08:36:30 +0800

    refactor: extract methods

--- src/svtp-web-portal-pack-template-items/Function.cs
+++ src/svtp-web-portal-pack-template-items/Function.cs
@@ -21,46 +21,74 @@ public class Function
     /// <returns></returns>
     public async Task<string> FunctionHandler(Parameter parameter, ILambdaContext context)
     {
-            using (var s3Client = new AmazonS3Client(RegionEndpoint.USWest2))
-            {
-                var result = await s3Client.ListObjectsV2Async(new ListObjectsV2Request
-                {
-                    BucketName = BucketName,
-                    Prefix = $"Templates/{parameter.TemplateVersion}/",
-                }, CancellationToken.None);
-                
-                var tasks = new List<Task<GetObjectResponse>>();
-                foreach (var s3Object in result.S3Objects)
-                {
-                    tasks.Add(s3Client.GetObjectAsync(BucketName, s3Object.Key, CancellationToken.None));
-                }
+        var files = await GetS3Files(parameter);
+        if (files.Length == 0)
+        {
+            throw new Exception($"Nothing found on S3. Parameters: {parameter}");
+        }
 
-                var files = await Task.WhenAll(tasks.ToArray());
-                await using var zipFileStream = new MemoryStream();
-                using var zipArchive = new ZipArchive(zipFileStream, ZipArchiveMode.Create);
-                foreach (var file in files)
-                {
-                    await AddToZip(zipArchive, file.Key.Replace("/", "_"), file.ResponseStream);
-                }
-                zipArchive.Dispose();
-                
-                var uploadZipFileKey = $"Download/{parameter.TemplateVersion}_{parameter.Project}_{parameter.Phase}_{DateTime.Now.ToString("yyyyMMddHmsffff")}.zip";
-                var uploadResult = await s3Client.PutObjectAsync(new PutObjectRequest
-                {
-                    BucketName = BucketName,
-                    Key = uploadZipFileKey,
-                    InputStream = new MemoryStream(zipFileStream.ToArray())
-                });
-                var preSignedUrl = s3Client.GetPreSignedURL(new GetPreSignedUrlRequest
-                {
-                    BucketName = BucketName,
-                    Key = uploadZipFileKey,
-                    Verb = HttpVerb.GET,
-                    Expires = DateTime.UtcNow.AddMinutes(30)
-                });
-                return preSignedUrl;
-            }
+        var zipFileStream = await Archive(files);
+        var preSignedUrl = await UploadZipFile(parameter, zipFileStream);
+        return preSignedUrl;
     }
+
+    private async Task<string> UploadZipFile(Parameter parameter, MemoryStream zipFileStream)
+    {
+        using (var s3Client = new AmazonS3Client(RegionEndpoint.USWest2))
+        {
+            var uploadZipFileKey =
+                $"Download/{parameter.TemplateVersion}_{parameter.Project}_{parameter.Phase}_{DateTime.Now.ToString("yyyyMMddHmsffff")}.zip";
+            var uploadResult = await s3Client.PutObjectAsync(new PutObjectRequest
+            {
+                BucketName = BucketName,
+                Key = uploadZipFileKey,
+                InputStream = new MemoryStream(zipFileStream.ToArray())
+            });
+            var preSignedUrl = s3Client.GetPreSignedURL(new GetPreSignedUrlRequest
+            {
+                BucketName = BucketName,
+                Key = uploadZipFileKey,
+                Verb = HttpVerb.GET,
+                Expires = DateTime.UtcNow.AddMinutes(30)
+            });
+            return preSignedUrl;
+        }
+    }
+
+    private async Task<MemoryStream> Archive(IEnumerable<GetObjectResponse> files)
+    {
+        await using var zipFileStream = new MemoryStream();
+        using var zipArchive = new ZipArchive(zipFileStream, ZipArchiveMode.Create);
+        foreach (var file in files)
+        {
+            await AddToZip(zipArchive, file.Key.Replace("/", "_"), file.ResponseStream);
+        }
+
+        zipArchive.Dispose();
+        return zipFileStream;
+    }
+
+    private async Task<GetObjectResponse[]> GetS3Files(Parameter parameter)
+    {
+        using (var s3Client = new AmazonS3Client(RegionEndpoint.USWest2))
+        {
+            var result = await s3Client.ListObjectsV2Async(new ListObjectsV2Request
+            {
+                BucketName = BucketName,
+                Prefix = $"Templates/{parameter.TemplateVersion}/",
+            }, CancellationToken.None);
+
+            var tasks = new List<Task<GetObjectResponse>>();
+            foreach (var s3Object in result.S3Objects)
+            {
+                tasks.Add(s3Client.GetObjectAsync(BucketName, s3Object.Key, CancellationToken.None));
+            }
+
+            var files = await Task.WhenAll(tasks.ToArray());
+            return files;
+        }
+    }
+
     private async Task AddToZip(ZipArchive zipArchive, string fileName, Stream fileStream)
     {
         var zipArchiveEntry = zipArchive.CreateEntry(fileName, CompressionLevel.Optimal);
